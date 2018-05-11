8960731409baf6c690dc942368e5c00f9742f3db
Ricardo Gosch <ricardo.gosch@pmweb.com.br>
2018-05-11 07:48:49 -0300

    First Commit

---
+++ src/api/todo/todo.js
@@ -0,0 +1,20 @@
+const restful = require('node-restful')
+const mongoose = restful.mongoose
+
+const todoSchema = new mongoose.Schema({
+    description: {
+        type: String,
+        required: true
+    },
+    done: {
+        type: Boolean,
+        default: false,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = restful.model('Todo', todoSchema)
